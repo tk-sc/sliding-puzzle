f079974ede0fabfea43375a1cf70087b22cc3427
tolhassianipar <jonathantolhas@gmail.com>
2020-12-19 00:51:57 +0700

    change variable name, add reference

--- algo/arr_helper.js
+++ algo/arr_helper.js
@@ -21,22 +21,22 @@ function shuffle(array) {
 }
 
 //taken from https://www.geeksforgeeks.org/check-instance-8-puzzle-solvable/
-function getInvCount(arr) {
-  let invCount = 0;
+function getTotalInversion(array) {
+  let totalInv = 0;
   for (let i = 0; i < 9 - 1; i++) {
     for (let j = i + 1; j < 9; j++) {
       // Value 8 is used for empty space
-      if (arr[j] != 8 && arr[i] != 8 && arr[i] > arr[j]) {
-        invCount++;
+      if (array[j] != 8 && array[i] != 8 && array[i] > array[j]) {
+        totalInv++;
       }
     }
   }
-  return invCount;
+  return totalInv;
 }
 
 //taken from https://www.geeksforgeeks.org/check-instance-8-puzzle-solvable/
 // This function returns true if given 8 puzzle is solvable.
-function isSolvable(arr) {
-  let invCount = getInvCount(arr);
-  return invCount % 2 == 0;
+function isSolvable(array) {
+  let totalInv = getTotalInversion(array);
+  return totalInv % 2 == 0;
 }
