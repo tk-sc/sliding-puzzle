c74c253ec957c397fd72aa5660121d9a5a3ae286
Adhytia <adit.14.12.99@gmail.com>
2020-12-21 01:17:35 +0700

    Added more detail to the description

--- soal.js
+++ soal.js
@@ -1,22 +1,61 @@
 datas = [{
     location:"images/batuk.jpeg",
     title:"Gejala Batuk",
-    description: "Batuk-batuk merupakan salah satu gejala pengidap COVID-19. Pada pasien Covid-19 gejala batuk kering memiliki persentase sebanyak 67,7 persen. Sementara batuk berdahak hanya dialami oleh 33,3 persen pasien."
+    description: `
+    Dalam kasus Covid-19, batuk umumnya dirasakan oleh sebagian besar pasien bergejala. Berdasarkan data dari Satuan Tugas (Satgas) Penanganan Covid-19, tercatat 70% pasien positif bergejala menderita batuk. <br>
+    <br>
+    <b>Bagaimana cara membedakan batuk karena Covid-19 dan batuk biasa? </b> <br>
+    Batuk biasa umumnya disertai dengan pilek dan didahului oleh fase bersin. Jika kamu mengalami hal demikian, maka kemungkinan kamu mengidap flu biasa. <br>
+    Mengingat Covid-19 mengiritasi jaringan paru-paru, maka batuk menjadi kering dan berkepanjangan. Saat virus mulai berkembang, jaringan paru-paru dipenuhi dengan cairan dan kamu akan merasa sesak napas karena tubuh berjuang untuk mendapatkan oksigen yang cukup. 
+    Jika menderita batuk kering dan mencurigai adanya infeksi virus corona, kamu harus mewaspadai gejala lain, seperti demam tinggi yang berlangsung lama, hilangnya rasa atau penciuman, nyeri otot, dan masalah pencernaan. Jika demikian, maka kamu disarankan untuk segera memeriksakan kondisimu ke dokter.<br>
+    <br>
+    Sumber : <br>
+    <a href="https://kesehatan.kontan.co.id/news/ini-perbedaan-batuk-karena-covid-19-dan-batuk-biasa?page=all" style="color: #da8844"> https://kesehatan.kontan.co.id/news/ini-perbedaan-batuk-karena-covid-19-dan-batuk-biasa?page=all </a>
+    `
 },
 {
-    location:"images/cuci_tangan.jpg",
+    location:"images/cuci_tangan.png",
     title:"Mencuci Tangan",
-    description: "Mencuci tangan selama 20 detik dapat mengurangi penyebaran virus COVID-19."
+    description: `
+    Penyebaran virus dari satu orang ke orang lain paling sering terjadi melalui tangan. Selama pandemi global terjadi, salah satu cara paling mudah, penting, dan tanpa biaya untuk mencegah penyebaran virus adalah dengan rutin cuci tangan menggunakan sabun dan air. Terdapat beberapa pertanyaan yang sering diajukan terkait mencuci tangan, seperti yang telah dirangkum berikut ini <br>
+    <br>
+    <b>Berapa lama saya harus mencuci tangan? </b> <br>
+    Kamu harus mencuci tangan setidaknya selama 20-30 detik. Tips cara mudah memperhitungkan durasinya, nyanyikan lagu 'Selamat Ulang Tahun' sebanyak dua kali. <br>
+    <br>
+    <b>Apakah saya membutuhkan air hangat untuk cuci tangan? </b> <br>
+    Tidak, Kamu bisa menggunakan air dengan temperatur apapun untuk cuci tangan. Air dingin atau air hangat sama-sama efektif dalam membunuh kuman atau virus  – asalkan kamu menggunakan sabun <br>
+    <br>
+    <b>Mana yang lebih baik: cuci tangan pakai sabun atau dengan cairan pembersih tangan? </b> <br>
+    Secara umum, cuci tangan, baik itu dengan air dan sabun, maupun dengan cairan pembersih tangan, ketika dilakukan dengan benar, keduanya sama efektifnya dalam membunuh kuman dan patogen. Gunakan cairan pembersih tangan yang berbasis alkohol minimal 60%. <br>
+    <br>
+    Sumber : <br>
+    <a href="https://www.unicef.org/indonesia/id/coronavirus/cuci-tangan" style="color: #da8844"> https://www.unicef.org/indonesia/id/coronavirus/cuci-tangan </a>
+    `
 },
 {
     location:"images/disinfektan.jpg",
     title:"Disinfektan",
-    description: "Disinfektan merupakan cairan yang dianggap dapat membunuh virus COVID-19, namun berbahaya apa bila dihirup secara langsung."
+    description: `
+    Disinfektan adalah bahan kimia yang digunakan untuk menghambat atau membunuh mikroorganisme (misalnya pada bakteri, virus dan jamur kecuali spora bakteri) pada permukaan benda mati, seperti furniture, ruangan, lantai,  dll. 
+    Disinfektan tidak digunakan pada  kulit maupun selaput lendir, karena berisiko mengiritasi kulit dan berpotensi memicu kanker. 
+    Jika terhirup, maka dapat menimbulkan bahaya dalam pernafasan mulai dari menimbulkan sesak nafas sampai iritasi paru-paru, sesuai banyaknya zat yang terhirup.
+    Hal ini berbeda dengan antiseptik yang memang ditujukan untuk disinfeksi pada  permukaan kulit dan membran mukosa. <br>
+    <br>
+    Sumber : <br>
+    <a href="https://farmasi.ugm.ac.id/id/cara-penggunaan-disinfektan-yang-tepat-untuk-mencegah-penyebaran-covid-19" style="color: #da8844"> https://farmasi.ugm.ac.id/id/cara-penggunaan-disinfektan-yang-tepat-untuk-mencegah-penyebaran-covid-19 </a>
+    `
 },
 {
-    location:"images/lockdown.jpeg",
+    location:"images/lockdown.jpg",
     title:"Lockdown",
-    description: "Lockdown merupakan kondisi di mana orang-orang tak boleh keluar dan masuk pada suatu daerah. Hal ini sangat membantu untuk menekan penyebaran virus COVID-19"
+    description: `
+    Lockdown adalah istilah yang digunakan untuk menjelaskan suatu upaya pengendalian penyebaran infeksi. 
+    Mengacu pada penjelasan Presiden Joko Widodo, lockdown mengharuskan sebuah wilayah menutup akses masuk maupun keluar sepenuhnya. 
+    Masyarakat di wilayah yang diberlakukan lockdown tidak dapat lagi keluar rumah dan berkumpul, sementara semua transportasi dan kegiatan perkantoran, sekolah, maupun ibadah akan dinonaktifkan. <br>
+    <br>
+    Sumber : <br>
+    <a href="https://www.alodokter.com/memahami-istilah-lockdown-yang-mencuat-di-tengah-pandemi-virus-corona" style="color: #da8844"> https://www.alodokter.com/memahami-istilah-lockdown-yang-mencuat-di-tengah-pandemi-virus-corona </a>
+    `
 },
 {
     location:"images/memakai-masker.jpg",
